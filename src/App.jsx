e3423e2c9d6b731ac11c2ed675272c6bb54b48e8
OrtegaF <ortegavaldesf@gmail.com>
2023-10-01 10:35:12 -0600

    Aplicación web que muestra un mapa ubicado en la Ciudad de Puebla, y puntos localizados

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+import MapView from './components/MapView'
+function App() {
+
+  return (
+    <MapView />
+  );
+}
+
+export default App
---
+++ src/components/MapView.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const MapView = () =>{
+    const Puebla = [19.03218, -98.21771];
+    const UTP = [19.05461, -98.15194];
+    const Loreto = [19.06556, -98.17693];
+    const Catedral = [19.04308, -98.19801];
+    const Fuente = [19.09676, -98.23289];
+return (
+       <div className='map-container'>
+    <MapContainer center={Puebla} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={UTP}>
+          <Popup>
+            Universidad Tecnológica de Puebla.
+          </Popup>
+        </Marker>
+        <Marker position={Loreto}>
+          <Popup>
+            Plaza Loreto, Puebla.
+          </Popup>
+        </Marker>
+        <Marker position={Catedral}>
+          <Popup>
+            Catedral de la Cd. de Puebla
+          </Popup>
+        </Marker>
+        <Marker position={Fuente}>
+          <Popup>
+            Fuente de los Frailes.
+          </Popup>
+        </Marker>
+    </MapContainer>
+    </div>
+)
+}
+
+export default MapView;
