29effb41633b4137b17308fe9db71fe2c004f0ea
Abigail6B <utp0145723@alumno.utpuebla.edu.mx>
2023-10-13 13:56:34 -0600

    Actualizaciones comentarios agregados

--- src/components/MapRutas.jsx
+++ src/components/MapRutas.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useMap } from 'react-leaflet'
-import L from "leaflet";
-import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import React from 'react' //Importacion de React
+import { useEffect } from 'react'//importacion de useEffect
+import { useMap } from 'react-leaflet' //Importacion de useMap
+import L from "leaflet"; //Libreria de Leaflet para trabajar con mapas interactivos
+
+//Extension de leaflet para las rutas y estilos de este (leaflet-routing-machine)
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css"; 
 import "leaflet-routing-machine";
+
+//Extension de leaflet para trabajar con el geodificador
 import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
 import 'leaflet-control-geocoder/dist/Control.Geocoder.js';
 
 
 export const MapRutas = () => {
+  //Declaracion del useMap para trabajar con el mapa que se tiene actualmente
   const map = useMap();
 
   useEffect(() => {
     if (!map) return;
 
+    //funcion que nos ayuda  a crear una instancia de un nuevo control de enrutamiento con las opciones proporcionadas a continuacion
     const routingControl = L.Routing.control({
+      //Se puede colocar una latitud y longitud por defecto si es que se necesita tener un marcado de ruta por defecto, si es que no, no se coloca nada
       waypoints: [L.latLng(), L.latLng()],
       routeWhileDragging: true,
+      //Con este se coloca los input para que se puedan colocar las direcciones y asi mismo las instrucciones para llegar a dicho punto de destino
       geocoder: L.Control.Geocoder.nominatim(),
+      //se cambia el lenguaje a español
       language: 'es',
     }).addTo(map);
 
+    //retornamos el mapa
     return () => map.removeControl(routingControl);
   }, [map]);
 
--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,21 +1,27 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import { MapRutas } from "./MapRutas";
+import React from "react"; //importamos React
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"; //Se importa la libreria react-leaflet con los componentes a utilizar
+import "leaflet/dist/leaflet.css"; //Se importan los estilos de react-leaflet
+import { MapRutas } from "./MapRutas"; //Se importa el archivo MapRutas.jsx el cual tiene el marcado de rutas
 
 const MapView = () =>{
-    const Puebla = [19.03218, -98.21771];
-    const UTP = [19.058392, -98.1545258];
-    const Loreto = [19.06556, -98.17693];
-    const Catedral = [19.04308, -98.19801];
-    const Fuente = [19.09676, -98.23289];
+    const Puebla = [19.03218, -98.21771]; //Se coloca la latitud y la longitud de Puebla
+    const UTP = [19.058392, -98.1545258]; //Se coloca la latitud y la longitud de la Universidad Tecnologica de Puebla
+    const Loreto = [19.06556, -98.17693]; //Se coloca la latitud y la longitud de los Fuertes de Lotero
+    const Catedral = [19.04308, -98.19801]; //Se coloca la latitud y la longitud de la Catedral de Puebla
+    const Fuente = [19.09676, -98.23289]; //Se coloca la latitud y la longitud de la Fuente de los Frailes
 return (
        <div className='map-container'>
-    <MapContainer center={Puebla} zoom={13} scrollWheelZoom={false}>
+        {/* Creacion del MapContainer donde se coloca la latitud y la longitud del estado de puebla y cuanto zoom
+        vamos a querer */}
+    <MapContainer center={Puebla} zoom={13} scrollWheelZoom={false}> 
+        {/* En el TitleLayer colocamos los derechos de autor */}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {/* Creacion de marcadores con el componente marker colocando en el position las variables
+        anteriores ya comentadas, asi mismo colocando una leyenda con la etiqueta Popup. Esto se repite
+        para las otras ubicaciones que definimos anteriormente*/}
         <Marker position={UTP}>
           <Popup>
             Universidad Tecnológica de Puebla.
@@ -36,6 +42,7 @@ return (
             Fuente de los Frailes.
           </Popup>
         </Marker>
+         {/* Llamado al archivo MapRutas */}
         <MapRutas/>
     </MapContainer>
     </div>
